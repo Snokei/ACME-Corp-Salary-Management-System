89c8540d4714b9956d980d212ebc158b235f591c
Neel <gmahor007@gmail.com>
2026-09-16 16:39:17 +0530

    deploy issue

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Login - ACME Salary Management System",
+  description: "Login to ACME Salary Management System",
+};
+
+export default function LoginLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,10 +7,6 @@ import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import toast from "react-hot-toast";
 
-export const metadata = {
-  title: "Login - ACME Salary Management System",
-  description: "Login to ACME Salary Management System",
-};
 
 export default function LoginPage() {
   const router = useRouter();
